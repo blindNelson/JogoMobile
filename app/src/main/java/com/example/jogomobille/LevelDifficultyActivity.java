27afef66171eadb2afe9cfa8c69e100513ccf971
Terada <teradaandre@gmail.com>
2021-11-20 23:32:24 -0300

    Telas

--- app/src/main/java/com/example/jogomobille/LevelDifficultyActivity.java
+++ app/src/main/java/com/example/jogomobille/LevelDifficultyActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
@@ -19,6 +21,14 @@ public class LevelDifficultyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set window to fullscreen (will hide status bar)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window = this.getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_level_difficulty);
 
         levelBar = findViewById(R.id.levelBar);
--- app/src/main/java/com/example/jogomobille/TelaDeMorteActivity.java
+++ app/src/main/java/com/example/jogomobille/TelaDeMorteActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class TelaDeMorteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set window to fullscreen (will hide status bar)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window = this.getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_tela_de_morte);
         Log.d("TelaDeMorte.java", "onCreate: sim");
     }
